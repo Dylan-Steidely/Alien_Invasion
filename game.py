3da15ec4b83974e66f7cb30e4d9308f9b5ab85fd
Dylan-Steidely <115735113+Dylan-Steidely@users.noreply.github.com>
2022-10-14 14:18:25 -0400

    on page

--- game.py
+++ game.py
@@ -1,39 +1,41 @@
 import sys
 
 import pygame
+
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
-    """ Overall Class to manage game assets and behavior"""
+    """Overall class to manage game assets and behavior."""
 
     def __init__(self):
-        """initialize the game, and create game resources"""
+        """Initialize the game, and create game resources."""
         pygame.init()
         self.settings = Settings()
 
         self.screen = pygame.display.set_mode(
-            (self.settings.screen_width,self.settings.screen_height))
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
-        # Set Background color
-        self.bg_color = (230,230,230)
+        self.ship = Ship(self)
+
 
     def run_game(self):
         """Start the main loop for the game."""
         while True:
-            #Watch for keyboard and mouse events.
+            # Watch for keyboard and mouse events.
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
 
-            #Redraw the screen during each pass through the loop.
+            # Redraw the screen during each pass through the loop.
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
 
-            #make The most recently drawn screen visible.
+            # Make the most recently drawn screen visible.
             pygame.display.flip()
 
-
 if __name__ == '__main__':
-    #Make a game instance, and run the game.
+    # Make a game instance, and run the game.
     ai = AlienInvasion()
     ai.run_game()
--- settings.py
+++ settings.py
@@ -4,6 +4,6 @@ class Settings:
     def __init__(self):
         """Initialize the game's settings."""
         # Screen settings
-        self.screen_width = 1900
-        self.screen_height = 1080
+        self.screen_width = 900
+        self.screen_height = 400
         self.bg_color = (230,230,230)
